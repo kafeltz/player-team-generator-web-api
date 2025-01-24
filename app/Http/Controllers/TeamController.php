d31383cbd74a140afad3d51dc95a69b7b6a7e8d9
Ismael S. Kafeltz <shalanga@gmail.com>
2025-01-24 08:37:11 -0300

    team controller rules

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TeamRequest;
 use App\Models\Player;
+use Exception;
 
 class TeamController extends Controller
 {
@@ -23,8 +24,32 @@ class TeamController extends Controller
         // ]
         $data = $request->all();
 
+        // rules
+        // 1: cannot be sent a request with a position with duplicated skill
+        self::checkDuplicatedSkillAndThrows($request);
+
         $players = Player::all();
 
         return response($players, 200);
+    }
+
+    private function checkDuplicatedSkillAndThrows(TeamRequest $request)
+    {
+        $data = $request->all();
+
+        $map = [];
+        foreach ($data as $value) {
+            $key = $value['position'].$value['mainSkill'];
+
+            if (! array_key_exists($key, $map)) {
+                $map[$key] = 1;
+            } else {
+                $map[$key]++;
+            }
+
+            if ($map[$key] > 1) {
+                throw new Exception('It cannot accept duplicated skill with the same position');
+            }
+        }
     }
 }
